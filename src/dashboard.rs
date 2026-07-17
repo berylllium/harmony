f3b23b7e27fa7637ac2abed924658030df9164a7
N. Mohanu <89540757+nathsys@users.noreply.github.com>
2026-07-18 01:10:36 +0200

    room list

--- src/dashboard.rs
+++ src/dashboard.rs
@@ -11,10 +11,7 @@ use gpui_component::{
 };
 
 use crate::{
-    assets::IconName,
-    dashboard::header::Header,
-    matrix::Matrix,
-    screen::{Screen, ScreenContainer},
+    assets::IconName, dashboard::header::Header, matrix::Matrix, screen::{Screen, ScreenContainer},
 };
 
 pub struct Dashboard {
@@ -37,6 +34,20 @@ impl Dashboard {
     pub fn view(window: &mut Window, cx: &mut App) -> Entity<Self> {
         cx.new(|cx| Self::new(window, cx))
     }
+
+    pub fn get_available_rooms(&self, cx: &App) -> Vec<String> {
+        self.matrix
+            .read(cx)
+            .rooms
+            .iter()
+            .map(|room| {
+                room.cached_display_name()
+                    .map(|name| name.to_string())
+                    .unwrap_or_else(|| room.room_id().to_string())
+            })
+            .collect()
+    }
+
 }
 
 impl Render for Dashboard {
@@ -55,14 +66,16 @@ impl Render for Dashboard {
                                 .child(SidebarGroup::new("Spaces").child(SidebarMenu::new().child(
                                     SidebarMenuItem::new("Test space 1").icon(IconName::House),
                                 )))
-                                .child(SidebarGroup::new("Rooms").child(SidebarMenu::new().child(
-                                    SidebarMenuItem::new("Test room 1")
-                                        .icon(IconName::Frame)
-                                        .on_click(cx.listener(|this, _, _, cx| {
-                                            this.screen_container.update(cx, |sc, cx| {
-                                                sc.set_screen(Screen::Chat);
-                                            });
-                                        })),
+                                .child(SidebarGroup::new("Rooms").child(SidebarMenu::new().children(
+                                    self.get_available_rooms(cx).into_iter().map(|room_name| {
+                                        SidebarMenuItem::new(room_name)
+                                            .icon(IconName::Frame)
+                                            .on_click(cx.listener(|this, _, _, cx| {
+                                                this.screen_container.update(cx, |sc, cx| {
+                                                    sc.set_screen(Screen::Chat);
+                                                });
+                                            }))
+                                    }),
                                 )))
                                 .footer(
                                     Button::new("settings-btn")
